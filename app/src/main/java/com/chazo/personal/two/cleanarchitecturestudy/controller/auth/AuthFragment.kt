6e82ad06653966946a7b0966fe5f9048691f0557
최재웅 <dwarf826@stunitas.com>
2019-03-21 12:18:21 +0900

    easyPermissions 라이브러리 삭제

--- app/src/main/java/com/chazo/personal/two/cleanarchitecturestudy/controller/auth/AuthFragment.kt
+++ app/src/main/java/com/chazo/personal/two/cleanarchitecturestudy/controller/auth/AuthFragment.kt
@@ -1,15 +1,17 @@
 package com.chazo.personal.two.cleanarchitecturestudy.controller.auth
 
-
 import android.Manifest
 import android.accounts.AccountManager
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.dagger.ktx.DaggerFragment
 import androidx.navigation.fragment.findNavController
 import com.chazo.personal.two.cleanarchitecturestudy.R
@@ -17,20 +19,17 @@ import com.chazo.personal.two.cleanarchitecturestudy.constant.RC_ACCOUNT_PICKER
 import com.chazo.personal.two.cleanarchitecturestudy.constant.RP_GET_ACCOUNTS
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import kotlinx.android.synthetic.main.fragment_auth.*
-import pub.devrel.easypermissions.AfterPermissionGranted
-import pub.devrel.easypermissions.EasyPermissions
 import javax.inject.Inject
 
-class AuthFragment() : DaggerFragment() {
+class AuthFragment : DaggerFragment() {
 
     @Inject
     lateinit var googleAccountCredential: GoogleAccountCredential
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_auth, container, false)
     }
 
@@ -39,27 +38,32 @@ class AuthFragment() : DaggerFragment() {
         button_sign_in.setOnClickListener { selectAccount() }
     }
 
-    @AfterPermissionGranted(RP_GET_ACCOUNTS)
     private fun selectAccount() {
-        val perms = arrayOf(Manifest.permission.GET_ACCOUNTS)
-        if (EasyPermissions.hasPermissions(requireContext(), *perms)) {
-            startActivityForResult(googleAccountCredential.newChooseAccountIntent(), RC_ACCOUNT_PICKER)
+        val accountPermission = Manifest.permission.GET_ACCOUNTS
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                accountPermission
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            showAccountPicker()
         } else {
-            requestPermission(perms)
+            requestPermissions(arrayOf(accountPermission), RP_GET_ACCOUNTS)
         }
     }
 
+    private fun showAccountPicker() {
+        startActivityForResult(googleAccountCredential.newChooseAccountIntent(), RC_ACCOUNT_PICKER)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_ACCOUNT_PICKER && resultCode == Activity.RESULT_OK) {
             data?.let {
                 val accountName = it.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
                 accountName?.let {
                     googleAccountCredential.selectedAccountName = it
                     moveToCalendarSelectFragment()
-                }?: run {
+                } ?: run {
                     Toast.makeText(requireContext(), "선택된 계정이 없습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -72,16 +76,16 @@ class AuthFragment() : DaggerFragment() {
         }
     }
 
-    private fun requestPermission(permissions: Array<String>) =
-            EasyPermissions.requestPermissions(
-                    this,
-                    "구글 계정을 가져오기 위해 필요합니다.",
-                    RP_GET_ACCOUNTS, *permissions
-            )
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+        when(requestCode) {
+            RP_GET_ACCOUNTS -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showAccountPicker()
+                } else {
+                    Toast.makeText(requireContext(), "정상적인 작동을 위해 앱 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
-
 }
